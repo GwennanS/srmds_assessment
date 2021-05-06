e20b5d379a2a78e6efac229268ee85f1909adf00
Nikki <n.h.bouman@student.tudelft.nl>
2021-05-06 10:59:39 +0200

    added density plot

--- courseworkA/part3-1.R
+++ courseworkA/part3-1.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(sm)
 
 # Get data
 filepath <- ("courseworkA/set0.csv")
@@ -6,14 +7,17 @@ ds <- read.csv(file=filepath, header=TRUE)
 ds <- data.frame(ds)
 
 # boxplot score overall distribution (session independent)
-boxplot(ds$score)
+#boxplot(ds$score)
 
 # set labels
 ds$sessionF <- factor(ds$session, levels=c(0:49), labels=c(0:49))
 
 # boxplot score per session
-boxplot(score~sessionF, data=ds, main="Score", xlab="session", ylab="score")
+#boxplot(score~sessionF, data=ds, main="Score", xlab="session", ylab="score")
+
+p <- ggplot(ds, aes(x=score)) + geom_density()
+p + geom_vline(aes(xintercept=mean(score)), color="blue", linetype="dashed",size=1)
 
 # ggplot score per session
-hp <- ggplot(ds, aes(x=session, y=score)) + geom_point(shape=1) + geom_smooth(method=lm)
-hp
+#hp <- ggplot(ds, aes(x=session, y=score)) + geom_point(shape=1) + geom_smooth(method=lm)
+#hp
