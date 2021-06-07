b59f4eb0f3d87b8c9446b9fd9eda52427b51ba00
NikkiBouman <n.h.bouman@student.tudelft.nl>
2021-06-07 13:53:35 +0200

    Update task1_alg1.m

--- courseworkC/task1_alg1.m
+++ courseworkC/task1_alg1.m
@@ -12,15 +12,24 @@ x_bar = 1/m * x_sum;       % use formula from slides
 y = x - x_bar;          % use formula from slides
 
 % 3. Compute the covariance matrix
-c = 1/n * y * y.';     % use formula from slides
+c = 1/n * y *  y.';     % use formula from slides
 
 % 4. Compute eigenvalues and eigenvectors of the covariance matrix
 [eigvec,eigval] = eig(c);
 
 a = 2;
-% modes
+% Compute the modes
 for  i = 1:size(eigvec,2)
     m_max(:,i) = a * eigvec(:,i) * sqrt(eigval(i,i)) + x_bar;
-    m_norm(:,i) = eigvec(:,i) * sqrt(eigval(i,i)) + x_bar;
     m_min(:,i) = -a * eigvec(:,i) * sqrt(eigval(i,i)) + x_bar;
 end
+
+% Visualize matrix
+figure
+scatter((c(:,end-1)*eigval(end,end)), (c(:, end)*eigval(end-1,end-1)))
+
+% Visualize modes
+figure
+steps = linspace(0,1,size(x_bar,1))';
+fill([steps;flipud(steps)],[m_min(:,1);flipud(m_max(:,1))],[.9 .9 .9],'linestyle','none');
+line(steps,x_bar)
